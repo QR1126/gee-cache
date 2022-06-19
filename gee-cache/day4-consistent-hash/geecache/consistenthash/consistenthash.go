944e8c3ec1851a7ebdbc8167f995a2612aee01f1
QR1126 <1193523825@qq.com>
2022-06-19 15:24:17 +0800

    Implement consistent hash

--- gee-cache/day4-consistent-hash/geecache/consistenthash/consistenthash.go
+++ gee-cache/day4-consistent-hash/geecache/consistenthash/consistenthash.go
@@ -19,9 +19,10 @@ type Map struct {
 
 // New creates a Map instance
 func New(replicas int, fn Hash) *Map {
+	// some code goes here
 	m := &Map{
-		replicas: replicas,
 		hash:     fn,
+		replicas: replicas,
 		hashMap:  make(map[int]string),
 	}
 	if m.hash == nil {
@@ -32,6 +33,7 @@ func New(replicas int, fn Hash) *Map {
 
 // Add adds some keys to the hash.
 func (m *Map) Add(keys ...string) {
+	// some code goes here
 	for _, key := range keys {
 		for i := 0; i < m.replicas; i++ {
 			hash := int(m.hash([]byte(strconv.Itoa(i) + key)))
@@ -44,15 +46,13 @@ func (m *Map) Add(keys ...string) {
 
 // Get gets the closest item in the hash to the provided key.
 func (m *Map) Get(key string) string {
+	// some code goes here
 	if len(m.keys) == 0 {
 		return ""
 	}
-
 	hash := int(m.hash([]byte(key)))
-	// Binary search for appropriate replica.
 	idx := sort.Search(len(m.keys), func(i int) bool {
 		return m.keys[i] >= hash
 	})
-
 	return m.hashMap[m.keys[idx%len(m.keys)]]
 }
